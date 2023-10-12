12f9ccd1b01649d19f733ab6ce88f2d29f7bc2c7
Janus Developer <dev@retrievo.net>
2023-10-12 10:51:12 -0400

    style active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,22 @@
+'use client';
+/**
+ * This is a client component because we are using
+ * usePathname() in this component. This uses browser APIs
+ * which only are accessible on client side
+ */
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
+import classNames from 'classnames';
 
 const NavBar = () => {
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' },
     ];
+
+    const currentPath = usePathname();
 
     return (
         <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -17,7 +27,11 @@ const NavBar = () => {
                 {links.map((link) => (
                     <li key={link.label}>
                         <Link
-                            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+                            className={classNames({
+                                'text-zinc-900': currentPath === link.href,
+                                'text-zinc-500': currentPath !== link.href,
+                                'hover:text-zinc-800 transition-colors': true,
+                            })}
                             href={link.href}
                         >
                             {link.label}

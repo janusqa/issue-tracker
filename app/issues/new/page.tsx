b8986be18bec5a02f6c42c49793f35452e4c58fe
Janus Developer <dev@retrievo.net>
2023-10-13 15:28:09 -0400

    handle new issue form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,16 +4,44 @@ import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { Button, TextField } from '@radix-ui/themes';
 import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { z } from 'zod';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+export const IssueFormSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1),
+});
+
+export interface IssueForm extends z.infer<typeof IssueFormSchema> {}
 
 const NewIssuePage = () => {
+    const router = useRouter();
+    const { register, control, handleSubmit } = useForm<IssueForm>();
     return (
-        <div className="max-w-xl space-y-3">
+        <form
+            className="max-w-xl space-y-3"
+            onSubmit={handleSubmit(async (data) => {
+                await axios.post('/api/issues', data);
+                router.push('/issues');
+            })}
+        >
             <TextField.Root>
-                <TextField.Input placeholder="title"></TextField.Input>
+                <TextField.Input
+                    placeholder="title"
+                    {...register('title')}
+                ></TextField.Input>
             </TextField.Root>
-            <SimpleMDE placeholder="Description" />
+            <Controller
+                name="description"
+                control={control}
+                render={({ field }) => (
+                    <SimpleMDE placeholder="Description" {...field} />
+                )}
+            />
             <Button>Submit New Issue</Button>
-        </div>
+        </form>
     );
 };
 

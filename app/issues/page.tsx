ba839fc8690873f633522d3aa8beb5d42c06ae57
Janus Developer <dev@retrievo.net>
2023-10-14 09:58:38 -0400

    add issue status badge

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Table } from '@radix-ui/themes';
 import Link from 'next/link';
 import prisma from '@/prisma/client';
+import IssueStatusBadge from '../components/IssueStatusBadge';
 
 const IssuesPage = async () => {
     const issues = await prisma.issue.findMany();
@@ -30,11 +31,11 @@ const IssuesPage = async () => {
                             <Table.ColumnHeaderCell>
                                 {issue.title}
                                 <div className="block md:hidden">
-                                    {issue.status}
+                                    <IssueStatusBadge status={issue.status} />
                                 </div>
                             </Table.ColumnHeaderCell>
                             <Table.ColumnHeaderCell className="hidden md:table-cell">
-                                {issue.status}
+                                <IssueStatusBadge status={issue.status} />
                             </Table.ColumnHeaderCell>
                             <Table.ColumnHeaderCell className="hidden md:table-cell">
                                 {issue.createdAt.toDateString()}

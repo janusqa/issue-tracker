3eaf8d6b1a337874ac9d5449d40f31f34b42a297
Janus Developer <dev@retrievo.net>
2023-10-14 10:18:54 -0400

    add loading skeletons for issues page

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import { Button, Table } from '@radix-ui/themes';
-import Link from 'next/link';
-import prisma from '@/prisma/client';
-import IssueStatusBadge from '../components/IssueStatusBadge';
+import { Table } from '@radix-ui/themes';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import IssueActions from './IssueActions';
 
-const IssuesPage = async () => {
-    const issues = await prisma.issue.findMany();
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+const LoadingIssuesPage = () => {
+    const issues = [1, 2, 3, 4, 5];
+
     return (
         <div>
             <IssueActions />
@@ -25,18 +23,18 @@ const IssuesPage = async () => {
                 </Table.Header>
                 <Table.Body>
                     {issues.map((issue) => (
-                        <Table.Row key={issue.id}>
+                        <Table.Row key={issue}>
                             <Table.ColumnHeaderCell>
-                                {issue.title}
+                                <Skeleton />
                                 <div className="block md:hidden">
-                                    <IssueStatusBadge status={issue.status} />
+                                    <Skeleton />
                                 </div>
                             </Table.ColumnHeaderCell>
                             <Table.ColumnHeaderCell className="hidden md:table-cell">
-                                <IssueStatusBadge status={issue.status} />
+                                <Skeleton />
                             </Table.ColumnHeaderCell>
                             <Table.ColumnHeaderCell className="hidden md:table-cell">
-                                {issue.createdAt.toDateString()}
+                                <Skeleton />
                             </Table.ColumnHeaderCell>
                         </Table.Row>
                     ))}
@@ -46,4 +44,4 @@ const IssuesPage = async () => {
     );
 };
 
-export default IssuesPage;
+export default LoadingIssuesPage;

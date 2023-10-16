f42ed24567259618671484d989235757a59f3803
Janus Developer <dev@retrievo.net>
2023-10-16 09:17:18 -0400

    refactor issue details page to apply single responsibility principle by putting the button and details in their own components and makeing the page responsible only for their layout

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,26 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+import { Issue } from '@prisma/client';
+import { Card, Heading, Text } from '@radix-ui/themes';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+interface Props {
+    issue: Issue;
+}
+
+const IssueDetails = ({ issue }: Props) => {
+    return (
+        <>
+            <Heading>{issue.title}</Heading>
+            <div className="flex space-x-3 my-2">
+                <IssueStatusBadge status={issue.status} />
+                <Text as="p">{issue.createdAt.toDateString()}</Text>
+            </div>
+            <Card className="prose mt-5">
+                <ReactMarkdown>{issue.description}</ReactMarkdown>
+            </Card>
+        </>
+    );
+};
+
+export default IssueDetails;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,8 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import prisma from '@/prisma/client';
-import { Box, Button, Card, Grid, Heading, Text } from '@radix-ui/themes';
+import { Box, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import ReactMarkdown from 'react-markdown';
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import Link from 'next/link';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 
 interface Props {
     params: { id: string };
@@ -21,20 +19,10 @@ const IssueDetailPage = async ({ params: { id } }: Props) => {
     return (
         <Grid columns={{ initial: '1', md: '2' }} gap="5">
             <Box>
-                <Heading>{issue.title}</Heading>
-                <div className="flex space-x-3 my-2">
-                    <IssueStatusBadge status={issue.status} />
-                    <Text as="p">{issue.createdAt.toDateString()}</Text>
-                </div>
-                <Card className="prose mt-5">
-                    <ReactMarkdown>{issue.description}</ReactMarkdown>
-                </Card>
+                <IssueDetails issue={issue} />
             </Box>
             <Box>
-                <Button>
-                    <Pencil2Icon />
-                    <Link href={`/issues/${id}/edit`}>Edit Issue</Link>
-                </Button>
+                <EditIssueButton issueId={issue.id} />
             </Box>
         </Grid>
     );

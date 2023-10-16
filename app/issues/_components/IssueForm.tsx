6620e9e9a306e2ac77828173b7488eab73497615
Janus Developer <dev@retrievo.net>
2023-10-16 09:40:04 -0400

    add edit issue page

---
+++ app/issues/_components/IssueForm.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+// import SimpleMDE from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
+import { Button, Callout, TextField, Text } from '@radix-ui/themes';
+import { useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { IssueValidationSchema } from '@/app/Validationchema';
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
+import { Issue } from '@prisma/client';
+
+// We lazy load to disable ssr so this MDE component is rendered on
+// client instead of server to avoide any bugs
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+    ssr: false,
+});
+
+export interface IssueFormData extends z.infer<typeof IssueValidationSchema> {}
+
+interface Props {
+    issue?: Issue;
+}
+
+const IssueForm = ({ issue }: Props) => {
+    const router = useRouter();
+    const {
+        register,
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IssueFormData>({
+        resolver: zodResolver(IssueValidationSchema),
+    });
+    const [error, setError] = useState('');
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleForm = async (data: IssueFormData) => {
+        try {
+            setIsSubmitting(true);
+            await axios.post('/api/issues', data);
+            router.push('/issues');
+        } catch (error) {
+            setIsSubmitting(false);
+            setError('An unexpected error occured');
+        }
+    };
+
+    return (
+        <div className="max-w-xl">
+            {error && (
+                <Callout.Root color="red" className="mb-5">
+                    <Callout.Text>
+                        You will need admin privileges to install and access
+                        this application.
+                    </Callout.Text>
+                </Callout.Root>
+            )}
+            <form className="space-y-3" onSubmit={handleSubmit(handleForm)}>
+                <TextField.Root>
+                    <TextField.Input
+                        defaultValue={issue?.title}
+                        placeholder="title"
+                        {...register('title')}
+                    ></TextField.Input>
+                </TextField.Root>
+                <ErrorMessage>{errors.title?.message}</ErrorMessage>
+                <Controller
+                    name="description"
+                    control={control}
+                    defaultValue={issue?.description}
+                    render={({ field }) => (
+                        <SimpleMDE placeholder="Description" {...field} />
+                    )}
+                />
+                <ErrorMessage>{errors.description?.message}</ErrorMessage>
+                <Button disabled={isSubmitting}>
+                    Submit New Issue {isSubmitting && <Spinner />}
+                </Button>
+            </form>
+        </div>
+    );
+};
+
+export default IssueForm;

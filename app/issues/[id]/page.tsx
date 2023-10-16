3dbf144ba61b1570d0f77a8372f28391244541cd
Janus Developer <dev@retrievo.net>
2023-10-16 08:54:50 -0400

    add edit button to issue details page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import prisma from '@/prisma/client';
-import { Card, Heading, Text } from '@radix-ui/themes';
+import { Box, Button, Card, Grid, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import Link from 'next/link';
 
 interface Props {
     params: { id: string };
@@ -17,16 +19,24 @@ const IssueDetailPage = async ({ params: { id } }: Props) => {
     if (!issue) notFound();
 
     return (
-        <div>
-            <Heading>{issue.title}</Heading>
-            <div className="flex space-x-3 my-2">
-                <IssueStatusBadge status={issue.status} />
-                <Text as="p">{issue.createdAt.toDateString()}</Text>
-            </div>
-            <Card className="prose mt-5">
-                <ReactMarkdown>{issue.description}</ReactMarkdown>
-            </Card>
-        </div>
+        <Grid columns={{ initial: '1', md: '2' }} gap="5">
+            <Box>
+                <Heading>{issue.title}</Heading>
+                <div className="flex space-x-3 my-2">
+                    <IssueStatusBadge status={issue.status} />
+                    <Text as="p">{issue.createdAt.toDateString()}</Text>
+                </div>
+                <Card className="prose mt-5">
+                    <ReactMarkdown>{issue.description}</ReactMarkdown>
+                </Card>
+            </Box>
+            <Box>
+                <Button>
+                    <Pencil2Icon />
+                    <Link href={`/issues/${id}/edit`}>Edit Issue</Link>
+                </Button>
+            </Box>
+        </Grid>
     );
 };
 

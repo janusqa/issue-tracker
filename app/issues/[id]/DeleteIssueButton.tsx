49716dbc6faac12b1287c6a3bcd0bc4350fee032
Janus Developer <dev@retrievo.net>
2023-10-17 18:48:00 -0400

    add delete an issue feature

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import { AlertDialog, Button } from '@radix-ui/themes';
+import axios from 'axios';
+import { useRouter } from 'next/navigation'; // this is router for the new apps dir
+// import { useRouter } from 'next/router'; // this is router from OLD pages navigation
 
 interface Props {
     issueId: number;
 }
 
 const DeleteIssueButton = ({ issueId }: Props) => {
+    const router = useRouter();
     return (
         <AlertDialog.Root>
             <AlertDialog.Trigger>
@@ -25,7 +29,16 @@ const DeleteIssueButton = ({ issueId }: Props) => {
                         </Button>
                     </AlertDialog.Cancel>
                     <AlertDialog.Action>
-                        <Button color="red">Delete Issue</Button>
+                        <Button
+                            color="red"
+                            onClick={async () => {
+                                await axios.delete(`/api/issues/${issueId}`);
+                                router.push('/issues');
+                                router.refresh();
+                            }}
+                        >
+                            Delete Issue
+                        </Button>
                     </AlertDialog.Action>
                 </div>
             </AlertDialog.Content>

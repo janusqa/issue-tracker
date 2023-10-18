abf08f1080e5ffc9f9cf298b584ab60cfbc345a5
Janus Developer <dev@retrievo.net>
2023-10-18 13:47:23 -0400

    add login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -9,6 +9,7 @@ import { usePathname } from 'next/navigation';
 import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
 import classNames from 'classnames';
+import { useSession } from 'next-auth/react';
 
 const NavBar = () => {
     const links = [
@@ -17,6 +18,8 @@ const NavBar = () => {
     ];
 
     const currentPath = usePathname();
+
+    const { status, data: session } = useSession();
 
     return (
         <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -39,6 +42,14 @@ const NavBar = () => {
                     </li>
                 ))}
             </ul>
+            <div>
+                {status === 'authenticated' && (
+                    <Link href="/api/auth/signout">Logout</Link>
+                )}{' '}
+                {status === 'unauthenticated' && (
+                    <Link href="/api/auth/signin">Login</Link>
+                )}
+            </div>
         </nav>
     );
 };

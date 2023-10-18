3e080fa4fb6864741485f640ed1d7244894420cb
Janus Developer <dev@retrievo.net>
2023-10-18 14:12:02 -0400

    refactor layout of nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -10,6 +10,7 @@ import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
 import classNames from 'classnames';
 import { useSession } from 'next-auth/react';
+import { Container } from '@radix-ui/themes';
 
 const NavBar = () => {
     const links = [
@@ -22,34 +23,43 @@ const NavBar = () => {
     const { status, data: session } = useSession();
 
     return (
-        <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-            <Link href="/">
-                <AiFillBug />
-            </Link>
-            <ul className="flex space-x-6">
-                {links.map((link) => (
-                    <li key={link.label}>
-                        <Link
-                            className={classNames({
-                                'text-zinc-900': currentPath === link.href,
-                                'text-zinc-500': currentPath !== link.href,
-                                'hover:text-zinc-800 transition-colors': true,
-                            })}
-                            href={link.href}
-                        >
-                            {link.label}
+        <nav className="border-b mb-5 px-5 py-3 ">
+            <Container>
+                <div className="flex justify-between">
+                    <div className="flex items-center gap-3">
+                        <Link href="/">
+                            <AiFillBug />
                         </Link>
-                    </li>
-                ))}
-            </ul>
-            <div>
-                {status === 'authenticated' && (
-                    <Link href="/api/auth/signout">Logout</Link>
-                )}{' '}
-                {status === 'unauthenticated' && (
-                    <Link href="/api/auth/signin">Login</Link>
-                )}
-            </div>
+                        <ul className="flex space-x-6">
+                            {links.map((link) => (
+                                <li key={link.label}>
+                                    <Link
+                                        className={classNames({
+                                            'text-zinc-900':
+                                                currentPath === link.href,
+                                            'text-zinc-500':
+                                                currentPath !== link.href,
+                                            'hover:text-zinc-800 transition-colors':
+                                                true,
+                                        })}
+                                        href={link.href}
+                                    >
+                                        {link.label}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div>
+                        {status === 'authenticated' && (
+                            <Link href="/api/auth/signout">Logout</Link>
+                        )}{' '}
+                        {status === 'unauthenticated' && (
+                            <Link href="/api/auth/signin">Login</Link>
+                        )}
+                    </div>
+                </div>
+            </Container>
         </nav>
     );
 };

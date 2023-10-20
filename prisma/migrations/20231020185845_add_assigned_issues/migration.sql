1e5012e9c4d510bcd014b8ab273fe1cb39586972
Janus Developer <dev@retrievo.net>
2023-10-20 14:59:14 -0400

    add assigned issues to prisma schema

---
+++ prisma/migrations/20231020185845_add_assigned_issues/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `Issue` ADD COLUMN `userId` VARCHAR(255) NULL;
+
+-- AddForeignKey
+ALTER TABLE `Issue` ADD CONSTRAINT `Issue_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;

e14cc4b7f90fcdea449cf71a94d8ab9303684251
Janus Developer <dev@retrievo.net>
2023-10-25 12:13:39 -0400

    add issue chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import prisma from '@/prisma/client';
 import IssueSummary from './IssueSummary';
 import LatestIssues from './LatestIssues';
+import IssueChart from './IssueChart';
 
 export default async function Home() {
     const open = await prisma.issue.count({ where: { status: 'OPEN' } });
@@ -11,6 +12,7 @@ export default async function Home() {
 
     return (
         <>
+            <IssueChart open={open} inProgress={inProgress} closed={closed} />
             <IssueSummary open={open} inProgress={inProgress} closed={closed} />
             <LatestIssues />;
         </>

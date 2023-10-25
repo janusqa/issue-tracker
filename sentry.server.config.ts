225f18757b68695a7ee409795709d6cb7f7e84ee
Janus Developer <dev@retrievo.net>
2023-10-25 13:02:44 -0400

    add sentry.io to project to track errors

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ca2e2c22bf9778beb3b810f837660b67@o4505738876944384.ingest.sentry.io/4506112024117248",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
